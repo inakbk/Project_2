6c1a781bca9b01c3d2c199e9e3057e6542456b59
Ina Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2015-09-18 20:00:45 +0200

    making a while loop to continue as long as max element of B is not "zero"

--- project_2/main.cpp
+++ project_2/main.cpp
@@ -33,59 +33,60 @@ int main()
     B.print();
 
     double tolerance = 0.00000001;
-    //finding the index(k,l) of the maximum element in B:
     double max_off_diagonal = 0;
     int k = 0;
     int l = 0;
-    for(int i=0, j=1; (i<=n_step-2) && (j<=n_step-2); ++i, ++j)
+
+    while(tolerance > max_off_diagonal)
     {
-        //Checking all off-diagonal elements:
-        if( ((B(i,j)*B(i,j)) > max_off_diagonal) && (i!=j) )
+        //finding the value and index(k,l) of the maximum element in B:
+        for(int i=0, j=1; (i<=n_step-2) && (j<=n_step-2); ++i, ++j)
         {
-            max_off_diagonal = B(i,j)*B(i,j);
-            k = i;
-            l = j;
-            cout << "here2" << endl;
+            //Checking all off-diagonal elements:
+            if( ((B(i,j)*B(i,j)) > max_off_diagonal) && (i!=j) )
+            {
+                max_off_diagonal = B(i,j)*B(i,j);
+                k = i;
+                l = j;
+                cout << "here2" << endl;
+            }
+
         }
+        cout << k << endl;
+        cout << l << endl;
+        cout << max_off_diagonal << endl;
+
+
+
+        //finding the values of c ans s (the S matrix):
+        double t = 0;
+        double tau = (B[l][l] - B[k][k])/(2*B[k][l]); //=cot(2*theta)
+
+        // ensuring that theta<=pi/4:
+        if(tau>0)
+        {
+            t = -tau + sqrt(1 + (tau*tau)); //set sqrt outside to compute only once?
+        }
+        if(tau<0)
+        {
+            t = -tau - sqrt(1 + (tau*tau));
+        }
+        double c = 1/sqrt(1 + (t*t));
+        double s = t*c;
+
+        //make a loop over i=0,1,..., and j=0,1,2,...,
+
+        B[i][i] = B[i][i]; //dont need to write?
+        double temp_B = B[i][k];
+        B[i][k] = B[i][k]*c - B[i][l]*s;
+        B[i][l] = B[i][l]*c - temp_B*s;
+        temp_B = B[k][k];
+        B[k][k] = B[k][k]*c*c - 2*B[k][l]*c*s + B[l][l]*s*s;
+        B[l][l] = B[l][l]*c*c - 2*B[k][l]*c*s + temp_B*s*s;
+        B[k][l] = 0; //or write the formula that should be zero?
 
     }
-    cout << k << endl;
-    cout << l << endl;
-    cout << max_off_diagonal << endl;
 
-
-
-
-
-    /*
-    //finding the values of c ans s (the S matrix):
-    double t = 0;
-    double tau = (B[l][l] - B[k][k])/(2*B[k][l]); //=cot(2*theta)
-
-    // ensuring that theta<=pi/4:
-    if(tau>0)
-    {
-        t = -tau + sqrt(1 + (tau*tau)); //set sqrt outside to compute only once?
-    }
-    if(tau<0)
-    {
-        t = -tau - sqrt(1 + (tau*tau));
-    }
-    double c = 1/sqrt(1 + (t*t));
-    double s = t*c;
-
-    //make a loop over i=0,1,..., and j=0,1,2,...,
-
-    B[i][i] = B[i][i]; //dont need to write?
-    double temp_B = B[i][k];
-    B[i][k] = B[i][k]*c - B[i][l]*s;
-    B[i][l] = B[i][l]*c - temp_B*s;
-    temp_B = B[k][k];
-    B[k][k] = B[k][k]*c*c - 2*B[k][l]*c*s + B[l][l]*s*s;
-    B[l][l] = B[l][l]*c*c - 2*B[k][l]*c*s + temp_B*s*s;
-    B[k][l] = 0; //or write the formula that should be zero?
-
-*/
 
     return 0;
 }
