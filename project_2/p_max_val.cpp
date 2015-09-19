da968023bf294cb146064b38eee5ffa23eade2bd
Ina Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2015-09-19 19:56:42 +0200

    p_max between 4 and 6 seems nice

--- project_2/p_max_val.cpp
+++ project_2/p_max_val.cpp
@@ -8,29 +8,48 @@ using namespace arma;
 
 int main()
 {
-    const int n_step = 100; //?
-    const double p_max = 5; //writing p instead of rho
-    const double p_min = 0;
+    int n = 10;
+    vec rho_max = linspace(4,6,n);
+    mat eigen_values = zeros<mat>(3,n);
+    mat A = zeros<mat>(3,n);
 
-    const double h = (p_max - p_min)/n_step;
-    vec p = linspace(p_min, p_max, n_step+1); //p_i = p_min + i*h
-    vec V = p%p;
-
-    // initializing B
-    double e = -1/(h*h); // all elements of the e vec is the same
-    vec d = 2/(h*h) + V;
-    //d.print();
-
-    mat B = zeros<mat>(n_step-1,n_step-1);
-    for(int i=0, j=1; (i<=n_step-2) && (j<=n_step-2); ++i, ++j)
+    for(int i=0; i<=n-1; i++)
     {
-        B(i,j) = e;
-        B(i,i) = d[i+1];
-        B(j,i) = e;
-    }
-    B(n_step-2,n_step-2) = d[n_step-1];
-    //B.print();
+        const int n_step = 10000; //?
+        const double p_max = rho_max[i];
+        const double p_min = 0;
 
-    vec eigval = eig_sym(B);
-    eigval.print();
+        const double h = (p_max - p_min)/n_step;
+        vec p = linspace(p_min, p_max, n_step+1); //p_i = p_min + i*h
+        vec V = p%p;
+
+        // initializing B
+        double e = -1/(h*h); // all elements of the e vec is the same
+        vec d = 2/(h*h) + V;
+
+        mat B = zeros<mat>(n_step-1,n_step-1);
+        for(int i=0, j=1; (i<=n_step-2) && (j<=n_step-2); ++i, ++j)
+        {
+            B(i,j) = e;
+            B(i,i) = d[i+1];
+            B(j,i) = e;
+        }
+        B(n_step-2,n_step-2) = d[n_step-1];
+
+        vec eigval = eig_sym(B);
+        eigen_values(0,i) = eigval[0];
+        A(0,i) = 3;
+        eigen_values(1,i) = eigval[1];
+        A(1,i) = 7;
+        eigen_values(2,i) = eigval[2];
+        A(2,i) = 11;
+        //eigval.print();
+    }
+
+    //A.print();
+    mat C = A - eigen_values;
+    //eigen_values.print();
+
+    C.print();
+
 }
