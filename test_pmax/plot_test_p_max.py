e0a6b14e66cc367595fbdcbff80b9de42781a8a2
Ina Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2015-10-24 14:09:37 +0200

    choice of p_max shown in the plots
    everything works fine

--- test_pmax/plot_test_p_max.py
+++ test_pmax/plot_test_p_max.py
@@ -34,27 +34,36 @@ def read_file(filename):
 """
 ------------------------------------------------------------------------------------------
 """
-P = 24
-p_max_list = linspace(3,8,P+1)
-n_step = 150
-
-diff_eigval = zeros(P+1)
+P = 14
+p_max_list = linspace(3,6,P+1)
+n_step = 100
+N = [50, 100, 150, 200]
 analytic_eigval = [3,7,11]
 
-for eigval_nr in [0,1,2]:
-	i = 0
-	for p_max in range(P+1):
-		eigval_jacobi, eigvec_jacobi_gs, time_jacobi = read_file("EigenValVecSolver_jacobi_pMax%s_nStep%s.txt" %(p_max,n_step))
-		diff_eigval[i] = abs(eigval_jacobi[eigval_nr] - analytic_eigval[eigval_nr])
-		i += 1
-	figure(1)	
-	plot(p_max_list, diff_eigval)
-	title('n_step = %s' %n_step)
-	xlabel("p_max")
-	ylabel("diff_eigval")
-	hold('on')
+#P = 24
+#p_max_list = linspace(3,8,P+1)
+#n_step = 150
 
-legend(["eigval 1", "eigval 2", "eigval 3"])
+k = 0
+for n_step in N:
+	diff_eigval = zeros(P+1)
+	for eigval_nr in [0,1,2]:
+		i = 0
+		for p_max in range(P+1):
+			eigval_jacobi, eigvec_jacobi_gs, time_jacobi = read_file("figures_p_max/EigenValVecSolver_jacobi_pMax%s_nStep%s.txt" %(p_max,n_step))
+			diff_eigval[i] = abs(eigval_jacobi[eigval_nr] - analytic_eigval[eigval_nr])
+			i += 1
+		figure(k)	
+		plot(p_max_list, diff_eigval)
+		title('n_step = %s' %n_step)
+		xlabel("p_max")
+		ylabel("diff_eigval")
+		hold('on')
+
+	plot(4.71428571,0.0005, "kx")
+	legend(["eigval 1", "eigval 2", "eigval 3", "chosen p_max"])
+	k += 1
+
 show()
 
 
