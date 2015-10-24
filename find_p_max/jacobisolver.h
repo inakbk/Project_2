1398bd2a988528479531b11a86a0db2e705d2708
Ina Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2015-10-24 11:16:58 +0200

    creating new test file to find p_max

---
+++ find_p_max/jacobisolver.h
@@ -0,0 +1,93 @@
+#ifndef JACOBISOLVER
+#define JACOBISOLVER
+
+#include <iostream>
+#include <cmath>
+#include <armadillo>
+#include <cstdlib>
+#include <time.h>
+#include <jacobi.h>
+
+class jacobisolver
+{
+public:
+    int n_step;
+    int maxNumberOfIterations;
+    mat B;
+    mat R;
+
+    // constructor, initiating input values and sets default values if not any given
+    jacobisolver(mat firstMatrix = zeros<mat>(2,2), int numberOfStep = 2, int maxIterations=1)
+    {
+        if(maxIterations == 1)
+        {
+            cout << "Not enough arguments given to jacobisolver. Aborting." << endl;
+            exit(1);
+        }
+        B = firstMatrix;
+        n_step = numberOfStep;
+        maxNumberOfIterations = maxIterations;
+        //Eigenvector initiated as identity
+        R = eye<mat>(n_step-1, n_step-1);
+    }
+
+    //solving and timing the whole jacobi algorithm:
+    void solve_w_jacobi_rotation(vec& eigval_jacobi, mat& eigvec_jacobi, int& numberOfIterations, int& converge_test, double& time_jacobi)
+    {
+        clock_t start_jacobi, finish_jacobi;
+        start_jacobi = clock();
+
+        iterations(eigval_jacobi, R, numberOfIterations, converge_test);
+        retriveSortEigenValVec(eigval_jacobi, eigvec_jacobi, B, R);
+
+        finish_jacobi = clock();
+        time_jacobi = ( (finish_jacobi - start_jacobi)/((double)CLOCKS_PER_SEC ) );
+    }
+
+    void iterations(vec& eigval_jacobi, mat& R, int& numberOfIterations, int& converge_test)
+    {
+        double tolerance = 1.0e-14;
+        double max_off_diagonal = 0;
+        int k = 0;
+        int l = 0;
+
+        //instantiating an object of class jacobi:
+        jacobi partsOfJacobiMethod;
+
+        partsOfJacobiMethod.find_max_elem_index(k, l, max_off_diagonal, B, n_step); //initial value for max_off_diagonal to enter loop
+
+        numberOfIterations = 0;
+        while(tolerance < max_off_diagonal)
+        {
+            if(++numberOfIterations > maxNumberOfIterations) {
+                cout << "Warning! Jacobi algorithm did not converge after " << maxNumberOfIterations << " iterations for n_step= " << n_step << ". Exiting jacobi rotation solver!" << endl;
+                converge_test = 0;
+                B = zeros<mat>(n_step-1,n_step-1);
+                break;
+            }
+            //finding the value and index(k,l) of the maximum element in B:
+            partsOfJacobiMethod.find_max_elem_index(k, l, max_off_diagonal, B, n_step);
+
+            //finding the values of c ans s (the S transformation matrix):
+            double c = 0;
+            double s = 0;
+            partsOfJacobiMethod.transformation_matrix(c, s, B, k, l);
+            //doing one transformation:
+            partsOfJacobiMethod.jacobi_rotation(B, R, c, s, k, l, n_step);
+        }
+    }
+
+    void retriveSortEigenValVec(vec& eigval_jacobi, mat& eigvec_jacobi, mat B, mat& R)
+    {
+        eigval_jacobi = B.diag();
+        eigval_jacobi = sort(eigval_jacobi);
+        uvec indexes_sorted_eigenval = sort_index(B.diag());
+        for(int i=0; i<=n_step-2; ++i)
+        {
+            eigvec_jacobi.col(i) = R.col(indexes_sorted_eigenval(i));
+        }
+    }
+};
+
+#endif // JACOBISOLVER
+
--- find_p_max/main.cpp
+++ find_p_max/main.cpp
@@ -1,43 +1,40 @@
-//Program to find the magnitude of p_max
-//-------------------------------------------------------------
-//    //Code for constructing test matrix
-//    mat B = ones<mat>(n_step-1, n_step-1);
-//    for(int i=0; (i<n_step-1); ++i)
-//    {
-//        B(i,i) = 0.0;
-//    }
-//    //B.print();
-
-
 #include <iostream>
+#include <cmath>
 #include <armadillo>
 #include <cstdlib>
 #include <time.h>
+#include <jacobi.h>
+#include <jacobisolver.h>
+#include <writetofile.h>
 
 using namespace std;
 using namespace arma;
 
-int main()
+int main(int argc, char *argv[])
 {
-    int n = 10;
-    vec rho_max = linspace(4,6,n);
-    mat eigen_values = zeros<mat>(3,n);
-    mat A = zeros<mat>(3,n);
-
-    for(int i=0; i<=n-1; i++)
+    if(argc < 4)
     {
-        const int n_step = 1000; //?
-        const double p_max = rho_max[i];
-        const double p_min = 0;
+        cout << "Not enough command line arguments given. "
+                "Give 3, in the following order: n_step, maximum number of iterations and p_max on command line." << endl;
+        cout << "Eks: >> ./main 10 10000 5" << endl;
+        exit(1);
+    }
+    else{
+        //variables from command line:
+        const int n_step = atof(argv[1]);
+        int maxNumberOfIterations = atof(argv[2]);
+        const double p_max = atof(argv[3]); //writing p instead of rho
 
+//-------------------------------------------------------------
+        //constructing matrix B:
+        const double p_min = 0;
         const double h = (p_max - p_min)/n_step;
         vec p = linspace(p_min, p_max, n_step+1); //p_i = p_min + i*h
         vec V = p%p;
 
-        // initializing B
+        // Constructing matrix B:
         double e = -1/(h*h); // all elements of the e vec is the same
         vec d = 2/(h*h) + V;
-
         mat B = zeros<mat>(n_step-1,n_step-1);
         for(int i=0, j=1; (i<=n_step-2) && (j<=n_step-2); ++i, ++j)
         {
@@ -46,21 +43,65 @@ int main()
             B(j,i) = e;
         }
         B(n_step-2,n_step-2) = d[n_step-1];
+//-------------------------------------------------------------
+        //clocking the operations:
+        clock_t start_arma, finish_arma;
+        start_arma = clock();
 
-        vec eigval = eig_sym(B);
-        eigen_values(0,i) = eigval[0];
-        A(0,i) = 3;
-        eigen_values(1,i) = eigval[1];
-        A(1,i) = 7;
-        eigen_values(2,i) = eigval[2];
-        A(2,i) = 11;
-        //eigval.print();
+        //solving equations with armadillo lib:
+        vec eigval_arma = zeros<vec>(n_step-1);
+        mat eigvec_arma = zeros<mat>(n_step-1,n_step-1);
+        eig_sym(eigval_arma, eigvec_arma, B);
+
+        finish_arma = clock();
+        double time_arma = ( (finish_arma - start_arma)/((double)CLOCKS_PER_SEC ) );
+
+        writetofile fileArma(eigval_arma, eigvec_arma.col(0), p_max, n_step, time_arma, "arma");
+
+//-------------------------------------------------------------
+        vec eigval_jacobi = zeros<vec>(n_step-1);
+        mat eigvec_jacobi = zeros<mat>(n_step-1,n_step-1);
+        int numberOfIterations = 0;
+        int converge_test = 1; //initializing to true, false if jacobi did not converge
+        double time_jacobi = 0;
+
+        //solving equations with jacobi rotation:
+        jacobisolver solveTestMatrix(B, n_step, maxNumberOfIterations);
+        solveTestMatrix.solve_w_jacobi_rotation(eigval_jacobi, eigvec_jacobi, numberOfIterations, converge_test, time_jacobi);
+
+        cout << "Total number of iterations with Jacobi rotation: " << numberOfIterations << endl;
+
+        cout << "Time Jacobi: " << time_jacobi << endl;
+        writetofile fileJacobi(eigval_jacobi, eigvec_jacobi.col(0), p_max, n_step, time_jacobi, "jacobi", numberOfIterations, converge_test);
+
+//-------------------------------------------------------------
+/*
+//        eigvec_arma.print();
+//        cout << "---" << endl;
+//        eigvec_jacobi.print();
+        cout << "eigenvaules:" << endl;
+//        eigval_arma.print();
+        cout << "----" << endl;
+        eigval_jacobi.print();
+        cout << "diff. eigenvec:" << endl;
+        //cout << size(eigvec_arma) << size(eigvec_jacobi) << endl;
+        vec a = eigvec_arma.col(0) - eigvec_jacobi.col(0);
+        a.print();
+        cout << "----" << endl;
+        cout << "diff. eigval:" << endl;
+        vec b = eigval_arma - eigval_jacobi;
+        b.print();
+
+        cout << eigval_jacobi[0] << endl;
+        cout << eigval_jacobi[1] << endl;
+        cout << eigval_jacobi[2] << endl;
+        */
+
+        cout << n_step << endl;
+        cout << size(eigval_arma) << endl;
+        cout << size(eigvec_arma) << endl;
+
     }
 
-    //A.print();
-    mat C = A - eigen_values;
-    //eigen_values.print();
-
-    C.print();
-
+    return 0;
 }
