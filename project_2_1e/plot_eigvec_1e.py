a89113872dfa6233ed45193269cd9d35c8a5c1d4
Ina Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2015-10-27 13:06:16 +0100

    plotting of eigvec for 1e works fine
    Next: plot for 2e

--- project_2_1e/plot_eigvec_1e.py
+++ project_2_1e/plot_eigvec_1e.py
@@ -36,14 +36,10 @@ def read_file(filename):
 ------------------------------------------------------------------------------------------
 """
 N = [150]#, 10, 50, 100]
-max_number_of_iterations = 100000
+#max_number_of_iterations = 100000
 p_max = 4.7
 
-#Running code:
 n_step = N[0]
-# not working
-#os.system('g++ -o project_2_1e/main project_2_1e/main.cpp -larmadillo -llapack -lblas')
-#os.system('./project_2_1e/main %s %s %s' %(n_step, max_number_of_iterations, p_max))
 
 eigval_arma, eigvec_arma_gs, time_arma = read_file("files/EigenValVecSolver_arma_pMax4_nStep%s.txt" %n_step)
 eigvec_arma_gs = array([0] + eigvec_arma_gs + [0])
@@ -54,60 +50,14 @@ eigvec_jacobi_gs = array([0] + eigvec_jacobi_gs + [0])
 abs_sq_psi_jacobi = eigvec_jacobi_gs*eigvec_jacobi_gs
 
 p_min = 0;
-h = (p_max - p_min)/n_step;
-p = linspace(p_min, p_max, n_step+1); #p_i = p_min + i*h
+h = (p_max - p_min)/n_step
+p = linspace(p_min, p_max, n_step+1)
 
 plot(p,abs_sq_psi_jacobi)
 #hold('on')
 #plot(p,abs_sq_psi_arma)
 show()
-#they are identical???
+#they are identical??? (arma and jacobi)
 
 
-"""
-N = [5]#, 10, 50, 100]
-max_number_of_iterations = 10000
-p_max = 5.
-
-#Running code:
-#n_step = N[2]
-
-for n_step in N:
-
-	#Running code
-	#os.system('g++ -o project_2/main project_2/main.cpp -larmadillo -llapack -lblas')
-	#os.system('./project_2/main %s %s %s' %(n_step, max_number_of_iterations, p_max))
-
-	FileName = 'EigvalSolver_jacobi_pMax%s_nStep%s.txt' %(int(p_max), n_step)
-
-	eigval, t = read_file(FileName)
-
-	#FirstEigenvalues = eigval[0:3]
-	FirstEigenvalues = [2.999999, 6.499999999, 11.44444445]
-	print FirstEigenvalues
-
-	AnalyticEigval = [3, 7, 11]
-
-
-	for i in range(3):
-		print FirstEigenvalues[i]
-		if FirstEigenvalues[i] > AnalyticEigval[i]:
-			if (round(FirstEigenvalues[i],4) - AnalyticEigval[i]) < 0.5:
-				print "yay"
-				#print "---"
-			else:
-				print "not good enough precition"
-				#print FirstEigenvalues[i] - AnalyticEigval[i]
-				#print "---"
-		if FirstEigenvalues[i] < AnalyticEigval[i]:
-			if (AnalyticEigval[i] - round(FirstEigenvalues[i],4)) <= 0.5:
-				#print AnalyticEigval[i] - FirstEigenvalues[i]
-				print "yay"
-				#print "---"
-			else:
-				print "not good enough precition"
-				#print AnalyticEigval[i] - FirstEigenvalues[i]
-				#print "---"
-
-"""
 
